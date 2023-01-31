ae443ef287c7d304787f5a816f7cf6464ba82526
Rachel Daniel <racheldaniel328@gmail.com>
2023-01-30 17:31:23 -0700

    added comments & styling

--- Mission04_red328/wwwroot/js/GradeCalculator.js
+++ Mission04_red328/wwwroot/js/GradeCalculator.js
@@ -1,5 +1,54 @@
 ﻿$("#btn").click(function () {
+    // declare variables
+    var assignments = parseInt($("#assignments").val());
+    var groupProject = parseInt($("#groupProject").val());
+    var quizzes = parseInt($("#quizzes").val());
+    var midterm = parseInt($("#midterm").val());
+    var final = parseInt($("#final").val());
+    var intex = parseInt($("#intex").val());
+    var finalGrade = (assignments * .5) + (groupProject * .1) + (quizzes * .1) + (midterm * .1) + (final * .1) + (intex * .1)
+    var letterGrade = ""
 
+    // if statements to calculate letter grade
+    if (finalGrade >= 94) {
+        letterGrade = 'A';
+    }
+    else if (finalGrade >= 90) {
+        letterGrade = 'A-';
+    }
+    else if (finalGrade >= 87) {
+        letterGrade = 'B+';
+    }
+    else if (finalGrade >= 84) {
+        letterGrade = 'B';
+    }
+    else if (finalGrade >= 80) {
+        letterGrade = 'B-';
+    }
+    else if (finalGrade >= 77) {
+        letterGrade = 'C+';
+    }
+    else if (finalGrade >= 74) {
+        letterGrade = 'C';
+    }
+    else if (finalGrade >= 70) {
+        letterGrade = 'C-';
+    }
+    else if (finalGrade >= 67) {
+        letterGrade = 'D+';
+    }
+    else if (finalGrade >= 64) {
+        letterGrade = 'D';
+    }
+    else if (finalGrade >= 60) {
+        letterGrade = 'D-';
+    }
+    else {
+        letterGrade = 'E';
+    }
 
-    $("#finalGrade").html(finalGrade)
-})
+    // Output final grade & letter grade; fade image away to focus on the grade
+    $("#finalGrade").html("Final Grade: " + finalGrade + "\nLetter Grade: " + letterGrade);
+    $("#imgRachel").fadeOut("slow");
+
+});
